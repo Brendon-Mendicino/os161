3d83cc4cb65a65231ec8070c971a8d21a0e5a1b2
Brendon Mendicino <brendonmendicino@yahoo.it>
2023-06-03 19:27:08 +0000

    removed spinlock from refcount

--- include/refcount.h
+++ include/refcount.h
@@ -4,6 +4,10 @@
 #include <types.h>
 #include <spinlock.h>
 
+#define WITH_SPINLOCK 0
+
+
+#if WITH_SPINLOCK
 typedef struct refcount {
     struct spinlock lock;
     unsigned int count;
@@ -73,5 +77,71 @@ static inline void refcount_add(unsigned int i, refcount_t *r)
     r->count += i;
     spinlock_release(&r->lock);
 }
+#else
+
+/*
+ * This structure is not atomic
+ */
+typedef struct refcount {
+    unsigned int count;
+} refcount_t;
+
+
+#define REFCOUNT_INIT(n)     { .count = (n) }
+
+
+static inline void INIT_REFCOUNT(refcount_t *refcount, unsigned int count)
+{
+    refcount->count = count;
+}
+
+static inline unsigned int refcount_read(refcount_t *r)
+{
+    unsigned int count;
+
+    count = r->count;
+
+    return count;
+}
+
+static inline void refcount_inc(refcount_t *r)
+{
+    r->count += 1;
+}
+
+static inline bool refcount_inc_not_zero(refcount_t *r)
+{
+    bool success = false;
+
+    if (r->count != 0) {
+        success = true;
+        r->count += 1;
+    }
+
+    return success;
+}
+
+static inline void refcount_dec(refcount_t *r)
+{
+    r->count -= 1;
+}
+
+static inline bool refcount_dec_not_zero(refcount_t *r)
+{
+    bool success = false;
+
+    if (r->count > 0) {
+        r->count -= 1;
+        success = true;
+    }
+
+    return success;
+}
+
+static inline void refcount_add(unsigned int i, refcount_t *r)
+{
+    r->count += i;
+}
+#endif
 
 #endif // _REFCOUNT_H_
